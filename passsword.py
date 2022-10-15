0c8ca236ecc3ad7ad8eb85f44262001e10360e68
roma380424227 <rzaika017@gmail.com>
2022-10-15 22:11:20 +0300

    do homework

---
+++ passsword.py
@@ -0,0 +1,15 @@
+ask_passowrd=123
+k=3
+while k>0:
+    k-=1
+    password=int(input())
+    if password==ask_passowrd:
+        print('Пароль принят')
+    elif password!=ask_passowrd:
+        print('ошибка')
+# я незнаю  как это делать,без элифа все работает ,но сним только одна попытка
+
+
+
+
+
